318df8bb7c2c176ca54d88ef546b44d5af36e867
Stonb <stonb@hotmail.com>
2022-07-17 00:59:56 -0400

    BAND endpoint: Created BAND POST endpoint

--- routes/band.js
+++ routes/band.js
@@ -3,14 +3,14 @@ var express = require('express');
 var router = express.Router();
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: env.DBUSER,
-  host: env.DBHOST,
-  database: env.DBDATABASE,
-  password: env.DBPASSWORD,
-  port: env.DBPORT,
+    user: env.DBUSER,
+    host: env.DBHOST,
+    database: env.DBDATABASE,
+    password: env.DBPASSWORD,
+    port: env.DBPORT,
 });
 
-// curl -X GET http://localhost:3000/user
+// curl -X GET http://localhost:3000/band
 router.get('/', (req,res) => {
     pool.query('SELECT * FROM band', (error, results) => {
         if (error) {
@@ -20,16 +20,33 @@ router.get('/', (req,res) => {
     })
 })
 
-// curl -X POST -H "Content-Type: application/json" -d '{"id":"5555","name":"ABC Ortho", "email":"info@abcortho.com"}' http://localhost:3000/user 
-router.post('/', (req,res) => {
-    if (users.length == 0)
+
+router.post('/', (req,response) => {
+    
+    const band = req.body.band;
+    const country = req.body.country;
+    const genre = req.body.genre;
+    const year = parseInt(req.body.year);
+
+    console.log(req.body);
+
+    if (band || country || genre || year)
     {
-        users.push(req.body);
-        res.send('USER:POST Received');
+        pgQuery = 'INSERT INTO band(band_name, country, genre, formed_date) VALUES($1,$2,$3,$4) RETURNING *';
+        pgValues =  [ band, country, genre, year];
+        
+        pool.query(pgQuery, pgValues, (err, res) => {
+            if (err) {
+                console.log(err.stack)
+            } else {
+                console.log(res.rows[0])
+                response.send(JSON.stringify({res:'USER:POST Received'}));
+            }
+        })
     }
     else
     {
-        res.send("SERVER MESSAGE: please use PUT instead!");
+        console.log("Wrong DATA");
     }
 })
 
